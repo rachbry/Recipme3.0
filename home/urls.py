e178f7cc5881f13b6bca8405004ac3871b19f563
rachbry <rach_cutler@live.co.uk>
2025-08-21 00:36:13 +0100

    Created homepage and installed allauth

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import Index
+from home.views import Index
+
+app_name = 'home'
 
 urlpatterns = [
     path ('', Index.as_view(), name='home')
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView
+from django.views import View
 
 # Create your views here.
 
