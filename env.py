5874a3e081463cf084d7e96af4ae283e80153eac
rachbry <rach_cutler@live.co.uk>
2025-08-21 22:43:42 +0100

    trying to connect neno database

--- env.py
+++ env.py
@@ -4,6 +4,8 @@ os.environ["SECRET_KEY"] = 'DeHftg345$£$th7'
 
 os.environ["DEVELOPMENT"] = 'True'
 
+os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_OqbNY9DtUJ8m@ep-dawn-firefly-abwgzj1g-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+
 PGHOST='ep-dawn-firefly-abwgzj1g-pooler.eu-west-2.aws.neon.tech'
 PGDATABASE='neondb'
 PGUSER='neondb_owner'
